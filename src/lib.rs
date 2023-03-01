cb14e0bc2d8bafc2b8e60584507a3ff7c74cb5df
DarumaDocker <DarumaDocker@gmail.com>
2023-03-01 18:47:56 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use github_flows::{
     get_octo, listen_to_event,
     octocrab::models::{events::payload::EventPayload, reactions::ReactionContent},
 };
-use slack_flows::send_message_to_channel;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -10,40 +9,23 @@ pub async fn run() {
     listen_to_event(
         "DarumaDockerOrg",
         "github-func-test",
-        vec!["issue_comment", "issues"],
+        vec!["issue_comment"],
         handler,
     )
     .await;
 }
 
 async fn handler(payload: EventPayload) {
-    let octo = get_octo(Some(String::from("DarumaDocker")));
-    let issues = octo.issues("DarumaDockerOrg", "github-func-test");
+    if let EventPayload::IssueCommentEvent(e) = payload {
+        let issue_number = e.comment.id.0;
 
-    let reaction = match payload {
-        EventPayload::IssuesEvent(e) => {
-            let issue_id = e.issue.number;
-            Some(
-                issues
-                    .create_reaction(issue_id, ReactionContent::Rocket)
-                    .await,
-            )
-        }
-        EventPayload::IssueCommentEvent(e) => {
-            let comment_id = e.comment.id.0;
-            Some(
-                issues
-                    .create_comment_reaction(comment_id, ReactionContent::Rocket)
-                    .await,
-            )
-        }
-        _ => None,
+        // installed app login
+        let octo = get_octo(Some(String::from("DarumaDockerOrg")));
+
+        let _reaction = octo
+            .issues("DarumaDockerOrg", "github-func-test")
+            .create_comment_reaction(issue_number, ReactionContent::Rocket)
+            .await
+            .unwrap();
     };
-
-    if let Some(re) = reaction {
-        match re {
-            Ok(c) => send_message_to_channel("reactor-space", "t1", c.created_at.to_rfc2822()),
-            Err(e) => send_message_to_channel("reactor-space", "t1", e.to_string()),
-        }
-    }
 }
