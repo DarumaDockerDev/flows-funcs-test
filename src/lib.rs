be2595c50d11bb96d7104423b5f17113ac36a7f8
DarumaDocker <DarumaDocker@gmail.com>
2023-03-12 14:28:27 +0800

    for test

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,14 @@
 use flowsnet_platform_sdk::write_error_log;
-use github_flows::{get_octo, listen_to_event, octocrab::models::events::payload::EventPayload};
 use openai_flows::chat_completion;
+use slack_flows::{listen_to_channel, send_message_to_channel, SlackMessage};
 
 #[no_mangle]
-#[tokio::main(flavor = "current_thread")]
-pub async fn run() {
-    listen_to_event(
-        "DarumaDockerDev",
-        "github-func-test",
-        vec!["issue_comment"],
-        handler,
-    )
-    .await;
+pub fn run() {
+    listen_to_channel("family-wangshi", "chat", handler);
 }
 
-async fn handler(payload: EventPayload) {
-    let octo = get_octo(Some(String::from("DarumaDockerDev")));
-    let issues = octo.issues("DarumaDockerDev", "github-func-test");
-
-    match payload {
-        EventPayload::IssueCommentEvent(e) => {
-            if e.comment.user.r#type != "Bot" {
-                if let Some(b) = e.comment.body {
-                    if let Some(r) =
-                        chat_completion("DarumaDocker", &format!("issue#{}", e.issue.number), &b)
-                    {
-                        if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
-                            write_error_log!(e.to_string());
-                        }
-                    }
-                }
-            }
-        }
-        _ => (),
-    };
+fn handler(sm: SlackMessage) {
+    if let Some(r) = chat_completion("Michael", "random", &sm.text) {
+        send_message_to_channel("family-wangshi", "chat", r.choice);
+    }
 }
