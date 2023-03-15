96b22165fbb7b23115e97c65893ecaa8a58617b2
DarumaDocker <DarumaDocker@gmail.com>
2023-03-15 10:28:41 +0800

    test for schedule

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,11 @@
 use flowsnet_platform_sdk::write_error_log;
-use github_flows::{get_octo, listen_to_event, octocrab::models::events::payload::EventPayload};
-use openai_flows::{chat_completion, ChatOptions};
+use schedule_flows::schedule_cron_job;
 
 #[no_mangle]
-#[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    listen_to_event(
-        "DarumaDockerDev",
-        "github-func-test",
-        vec!["issue_comment"],
-        handler,
-    )
-    .await;
+    schedule_cron_job(String::from("0 10 * * *"), String::from("no"), handler);
 }
 
-async fn handler(payload: EventPayload) {
-    let octo = get_octo(Some(String::from("DarumaDockerDev")));
-    let issues = octo.issues("DarumaDockerDev", "github-func-test");
-
-    match payload {
-        EventPayload::IssueCommentEvent(e) => {
-            if e.comment.user.r#type != "Bot" {
-                if let Some(b) = e.comment.body {
-                    let co = ChatOptions {
-                        restart: b.eq_ignore_ascii_case("/restart"),
-                        restarted_sentence: Some("let's restart a talk"),
-                    };
-                    if let Some(r) = chat_completion(
-                        "DarumaDocker",
-                        &format!("issue#{}", e.issue.number),
-                        &b,
-                        &co,
-                    ) {
-                        write_error_log!(r.restarted);
-                        if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
-                            write_error_log!(e.to_string());
-                        }
-                    }
-                }
-            }
-        }
-        _ => (),
-    };
+fn handler(payload: Vec<u8>) {
+    write_error_log!(payload);
 }
