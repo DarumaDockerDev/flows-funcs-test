54ac0857a145719834322863d34871000741fb11
DarumaDocker <DarumaDocker@gmail.com>
2023-03-15 10:49:14 +0800

    test for schedule

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use flowsnet_platform_sdk::write_error_log;
 use schedule_flows::schedule_cron_job;
 
 #[no_mangle]
-pub async fn run() {
+pub fn run() {
     schedule_cron_job(String::from("0 10 * * *"), String::from("no"), handler);
 }
 
