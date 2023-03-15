eafaf25967ccd86756631c31a9a48facb7f509a3
DarumaDocker <DarumaDocker@gmail.com>
2023-03-15 15:40:46 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use schedule_flows::schedule_cron_job;
 
 #[no_mangle]
 pub fn run() {
-    schedule_cron_job(String::from("0 * * * *"), String::from("no"), handler);
+    schedule_cron_job(String::from("2 * * * *"), String::from("no"), handler);
 }
 
 fn handler(payload: Vec<u8>) {
