e2824986b5f8de4f42a264fd0a726b4fb6ffcaed
DarumaDocker <DarumaDocker@gmail.com>
2023-03-20 13:25:24 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,12 @@ pub fn run() {
     let tele = Telegram::new(telegram_token.clone());
 
     listen_to_update(telegram_token, |update| {
-        println!("---1");
         if let UpdateKind::Message(msg) = update.kind {
             let text = msg.text().unwrap_or("");
             let chat_id = msg.chat.id;
 
             let message = tele.send_message(chat_id, text);
 
-            println!("---2");
             match message {
                 Ok(m) => {
                     let c = chat_completion(
@@ -26,13 +24,12 @@ pub fn run() {
                         &ChatOptions::default(),
                     );
 
-                    println!("---3");
                     if let Some(c) = c {
                         if c.restarted {
                             _ = tele.send_message(chat_id, "Let's start a new conversation!");
                         }
 
-                        _ = tele.edit_message_text(chat_id, m.id, c.choice);
+                        // _ = tele.edit_message_text(chat_id, m.id, c.choice);
                     } else {
                         _ = tele.send_message(chat_id, "I have no choice");
                     }
