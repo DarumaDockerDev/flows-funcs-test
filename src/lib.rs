bec9cc315fc4874297aab71516a915a588a89b32
DarumaDocker <DarumaDocker@gmail.com>
2023-03-30 18:43:01 +0800

    test for dm

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-use discord_flows::{create_text_message_in_channel, listen_to_channel, TextMessage};
+use discord_flows::{create_text_message_in_dm, listen_to_dm, TextMessage};
 
 #[no_mangle]
 pub fn run() {
-    listen_to_channel("ServerDev", "general", handler);
+    listen_to_dm("DarumaDockerDev", handler);
 }
 
 fn handler(tm: TextMessage) {
     if !tm.author.bot {
-        create_text_message_in_channel("ServerDev", "general", tm.content, Some(tm.id));
+        create_text_message_in_dm("DarumaDockerDev", tm.content, Some(tm.id));
     }
 }
