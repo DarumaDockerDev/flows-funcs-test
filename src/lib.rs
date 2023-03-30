6c72e898070426eeddb520bb581a4f509ac32828
DarumaDocker <DarumaDocker@gmail.com>
2023-03-30 11:26:14 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use discord_flows::{create_text_message_in_channel, listen_to_channel, TextMessage};
 
 #[no_mangle]
-pub async fn run() {
+pub fn run() {
     listen_to_channel("ServerDev", "general", handler);
 }
 
