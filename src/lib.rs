ef66ca12529231a94f3086392cf31fc27aff9a99
DarumaDocker <DarumaDocker@gmail.com>
2023-03-30 11:19:06 +0800

    test for discord

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,10 @@
-use github_flows::{
-    get_octo, listen_to_event,
-    octocrab::models::{events::payload::EventPayload, reactions::ReactionContent},
-};
+use discord_flows::{create_text_message_in_channel, listen_to_channel, TextMessage};
 
 #[no_mangle]
-#[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    listen_to_event(
-        "DarumaDockerDev",
-        "github-func-test",
-        vec!["issue_comment"],
-        handler,
-    )
-    .await;
+    listen_to_channel("ServerDev", "general", handler);
 }
 
-async fn handler(payload: EventPayload) {
-    if let EventPayload::IssueCommentEvent(e) = payload {
-        let issue_number = e.comment.id.0;
-
-        // installed app login
-        let octo = get_octo(Some(String::from("DarumaDockerDev")));
-
-        let _reaction = octo
-            .issues("DarumaDockerDev", "github-func-test")
-            .create_reaction(issue_number, ReactionContent::Rocket)
-            .await
-            .unwrap();
-    };
+fn handler(tm: TextMessage) {
+    create_text_message_in_channel("ServerDev", "general", tm.content);
 }
