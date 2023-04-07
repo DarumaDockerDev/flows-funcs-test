a1387e5ae9ab03196e60a6a71bc0167e8f2867e5
DarumaDocker <DarumaDocker@gmail.com>
2023-04-07 22:41:23 +0800

    test for openai's create image

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use discord_flows::{create_text_message_in_dm, listen_to_dm, TextMessage};
+use openai_flows::{create_image, ImageRequest, ImageSize};
 
 #[no_mangle]
 pub fn run() {
@@ -7,6 +8,19 @@ pub fn run() {
 
 fn handler(tm: TextMessage) {
     if !tm.author.bot {
-        create_text_message_in_dm("DarumaDockerDev", tm.content, Some(tm.id));
+        let ir = ImageRequest {
+            prompt: tm.content,
+            n: 2,
+            size: ImageSize::S256,
+            retry_times: 2,
+        };
+        let r = create_image("Michael", ir);
+
+        match r.len() {
+            0 => {
+                create_text_message_in_dm("DarumaDockerDev", String::from("No images"), Some(tm.id))
+            }
+            _ => create_text_message_in_dm("DarumaDockerDev", r[0].clone(), Some(tm.id)),
+        }
     }
 }
