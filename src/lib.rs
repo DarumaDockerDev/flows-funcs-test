ba075047dd1199f0f1c617e1457a45cd972be975
DarumaDocker <DarumaDocker@gmail.com>
2023-04-15 22:06:52 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use discord_flows::{create_text_message_in_dm, listen_to_dm, TextMessage};
-use openai_flows::{create_image_default_key, ImageRequest, ImageSize};
 
 extern "C" {
     fn set_log(p: *const u8, len: i32);
@@ -11,25 +10,12 @@ pub fn run() {
 }
 
 fn handler(tm: TextMessage) {
-    let log = "abc";
+    let log = b"abc".to_vec();
     unsafe {
         set_log(log.as_ptr(), log.len() as i32);
     }
 
     if !tm.author.bot {
-        let ir = ImageRequest {
-            prompt: tm.content,
-            n: 2,
-            size: ImageSize::S256,
-            retry_times: 2,
-        };
-        let r = create_image_default_key(ir);
-
-        match r.len() {
-            0 => {
-                create_text_message_in_dm("DarumaDockerDev", String::from("No images"), Some(tm.id))
-            }
-            _ => create_text_message_in_dm("DarumaDockerDev", r[0].clone(), Some(tm.id)),
-        }
+        create_text_message_in_dm("DarumaDockerDev", tm.content, Some(tm.id))
     }
 }
