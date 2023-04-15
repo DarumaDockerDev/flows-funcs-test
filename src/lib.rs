4a67320880f68f072d05017cd9d4807c4b905867
DarumaDocker <DarumaDocker@gmail.com>
2023-04-15 22:36:38 +0800

    test for debug log

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
 use cloud_vision_flows::text_detection;
 use lambda_flows::{request_received, send_response};
 
+extern "C" {
+    fn set_log(p: *const u8, len: i32);
+}
+
 #[no_mangle]
 pub fn run() {
     request_received(|_qry, body| {
+        let log = b"abc".to_vec();
+        unsafe {
+            set_log(log.as_ptr(), log.len() as i32);
+        }
+
         let text = text_detection(String::from_utf8(body).unwrap());
         match text {
             Ok(r) => send_response(
