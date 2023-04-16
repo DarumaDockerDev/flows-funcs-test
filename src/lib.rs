c7d816a7264603906114df99413e3c1a24348899
DarumaDocker <DarumaDocker@gmail.com>
2023-04-16 21:49:48 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use cloud_vision_flows::text_detection;
 use flowsnet_platform_sdk::logger;
 use flowsnet_platform_sdk::{debug, error, info, warn};
 use lambda_flows::{request_received, send_response};
@@ -10,13 +11,14 @@ pub fn run() {
         info!("info \ninfo");
         warn!("warn \nwarn");
         error!("error \nerror");
+        let text = text_detection(String::from_utf8(body).unwrap());
         send_response(
             200,
             vec![(
                 String::from("content-type"),
                 String::from("text/plain; charset=UTF-8"),
             )],
-            body,
+            text.unwrap().as_bytes().to_vec(),
         );
     });
 }
