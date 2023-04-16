50a95ed143a92fc2c1515ba172d16c71a990bbe3
DarumaDocker <DarumaDocker@gmail.com>
2023-04-16 20:48:23 +0800

    test for log

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+use flowsnet_platform_sdk::logger;
+use flowsnet_platform_sdk::{debug, error, info, warn};
 use lambda_flows::{request_received, send_response};
 
 #[no_mangle]
 pub fn run() {
+    logger::init();
     request_received(|_qry, body| {
+        debug!("debug \ndebug");
+        info!("info \ninfo");
+        warn!("warn \nwarn");
+        error!("error \nerror");
         send_response(
             200,
             vec![(
