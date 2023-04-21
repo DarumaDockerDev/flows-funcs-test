9cfcc122639f9f20e94bd351907fe1f11f21a79f
DarumaDocker <DarumaDocker@gmail.com>
2023-04-21 14:20:18 +0800

    test for new log using fern

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
-use cloud_vision_flows::text_detection;
+// use cloud_vision_flows::text_detection;
 use flowsnet_platform_sdk::logger;
-use flowsnet_platform_sdk::{debug, error, info, warn};
 use lambda_flows::{request_received, send_response};
 
 #[no_mangle]
 pub fn run() {
     logger::init();
     request_received(|_qry, body| {
-        debug!("debug \ndebug");
-        info!("info \ninfo");
-        warn!("warn \nwarn");
-        error!("error \nerror");
-        let text = text_detection(String::from_utf8(body).unwrap());
+        log::debug!("debug \ndebug");
+        log::info!("info \ninfo");
+        log::warn!("warn \nwarn");
+        log::error!("error \nerror");
+        // let text = text_detection(String::from_utf8(body).unwrap());
         send_response(
             200,
             vec![(
                 String::from("content-type"),
                 String::from("text/plain; charset=UTF-8"),
             )],
-            text.unwrap().as_bytes().to_vec(),
+            // text.unwrap().as_bytes().to_vec(),
+            body,
         );
     });
 }
