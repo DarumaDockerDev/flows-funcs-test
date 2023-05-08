65372f011e76e0152bb6a416aa74c50bcb412f97
DarumaDocker <DarumaDocker@gmail.com>
2023-05-09 07:41:41 +0800

    test

--- src/github-pr-review.rs
+++ src/github-pr-review.rs
@@ -1,15 +1,19 @@
 use dotenv::dotenv;
-use flowsnet_platform_sdk::write_error_log;
+use flowsnet_platform_sdk::logger;
 use github_flows::{
     get_octo, listen_to_event,
     octocrab::models::events::payload::{IssueCommentEventAction, PullRequestEventAction},
-    EventPayload,
+    octocrab::models::CommentId,
+    EventPayload, GithubLogin,
 };
 use http_req::{
     request::{Method, Request},
     uri::Uri,
 };
-use openai_flows::{chat_completion_default_key, ChatModel, ChatOptions};
+use openai_flows::{
+    chat::{ChatModel, ChatOptions},
+    OpenAIFlows,
+};
 use std::env;
 
 //  The soft character limit of the input context size
@@ -23,32 +27,34 @@ static MODEL: ChatModel = ChatModel::GPT35Turbo;
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
     dotenv().ok();
+    logger::init();
+    log::debug!("Running github-pr-review/main");
 
-    let login = env::var("login").unwrap_or("DarumaDockerDev".to_string());
-    let owner = env::var("owner").unwrap_or("DarumaDockerDev".to_string());
-    let repo = env::var("repo").unwrap_or("PrTest".to_string());
+    let owner = env::var("github_owner").unwrap_or("DarumaDocker".to_string());
+    let repo = env::var("github_repo").unwrap_or("Test".to_string());
     let trigger_phrase = env::var("trigger_phrase").unwrap_or("flows review".to_string());
 
     let events = vec!["pull_request", "issue_comment"];
-    listen_to_event(&login, &owner, &repo, events, |payload| {
-        handler(&login, &owner, &repo, &trigger_phrase, payload)
+    listen_to_event(&GithubLogin::Default, &owner, &repo, events, |payload| {
+        handler(&owner, &repo, &trigger_phrase, payload)
     })
     .await;
 
     Ok(())
 }
 
-async fn handler(
-    login: &str,
-    owner: &str,
-    repo: &str,
-    trigger_phrase: &str,
-    payload: EventPayload,
-) {
+async fn handler(owner: &str, repo: &str, trigger_phrase: &str, payload: EventPayload) {
+    // log::debug!("Received payload: {:?}", payload);
+    let mut new_commit: bool = false;
     let (title, pull_number, _contributor) = match payload {
         EventPayload::PullRequestEvent(e) => {
-            if e.action != PullRequestEventAction::Opened {
-                write_error_log!("Not a Opened pull event");
+            if e.action == PullRequestEventAction::Opened {
+                log::debug!("Received payload: PR Opened");
+            } else if e.action == PullRequestEventAction::Synchronize {
+                new_commit = true;
+                log::debug!("Received payload: PR Synced");
+            } else {
+                log::debug!("Not a Opened or Synchronize event for PR");
                 return;
             }
             let p = e.pull_request;
@@ -60,9 +66,10 @@ async fn handler(
         }
         EventPayload::IssueCommentEvent(e) => {
             if e.action == IssueCommentEventAction::Deleted {
-                write_error_log!("Deleted issue event");
+                log::debug!("Deleted issue comment");
                 return;
             }
+            log::debug!("Other event for issue comment");
 
             let body = e.comment.body.unwrap_or_default();
 
@@ -70,13 +77,13 @@ async fn handler(
             //     return;
             // }
             // TODO: Makeshift but operational
-            if body.starts_with("Hello, I am a [serverless review bot]") {
-                write_error_log!("Ignore comment via bot");
+            if body.starts_with("Hello, I am a [code review bot]") {
+                log::info!("Ignore comment via bot");
                 return;
             };
 
             if !body.to_lowercase().contains(&trigger_phrase.to_lowercase()) {
-                write_error_log!(format!("Ignore the comment, raw: {}", body));
+                log::info!("Ignore the comment without magic words");
                 return;
             }
 
@@ -87,17 +94,57 @@ async fn handler(
 
     let chat_id = format!("PR#{pull_number}");
     let system = &format!("You are a senior software developer. You will review a source code file and its patch related to the subject of \"{}\".", title);
+    let mut openai = OpenAIFlows::new();
+    openai.set_retry_times(3);
 
-    let octo = get_octo(Some(String::from(login)));
+    let octo = get_octo(&GithubLogin::Default);
+    let issues = octo.issues(owner, repo);
+    let mut comment_id: CommentId = 0u64.into();
+    if new_commit {
+        // Find the first "Hello, I am a [code review bot]" comment to update
+        match issues.list_comments(pull_number).send().await {
+            Ok(comments) => {
+                for c in comments.items {
+                    if c.body
+                        .unwrap_or_default()
+                        .starts_with("Hello, I am a [code review bot]")
+                    {
+                        comment_id = c.id;
+                        break;
+                    }
+                }
+            }
+            Err(error) => {
+                log::error!("Error getting comments: {}", error);
+                return;
+            }
+        }
+    } else {
+        // PR OPEN or Trigger phrase: create a new comment
+        match issues.create_comment(pull_number, "Hello, I am a [code review bot](https://github.com/flows-network/github-pr-review/) on [flows.network](https://flows.network/).\n\nIt could take a few minutes for me to analyze this PR. Relax, grab a cup of coffee and check back later. Thanks!").await {
+            Ok(comment) => {
+                comment_id = comment.id;
+            }
+            Err(error) => {
+                log::error!("Error posting comment: {}", error);
+                return;
+            }
+        }
+    }
+    if comment_id == 0u64.into() {
+        return;
+    }
+
     let pulls = octo.pulls(owner, repo);
     let mut resp = String::new();
-    resp.push_str("Hello, I am a [serverless review bot](https://github.com/flows-network/github-pr-review/) on [flows.network](https://flows.network/). Here are my reviews of changed source code files in this PR.\n\n------\n\n");
+    resp.push_str("Hello, I am a [code review bot](https://github.com/flows-network/github-pr-review/) on [flows.network](https://flows.network/). Here are my reviews of changed source code files in this PR.\n\n------\n\n");
     match pulls.list_files(pull_number).await {
         Ok(files) => {
             for f in files.items {
                 let filename = &f.filename;
                 if filename.ends_with(".md")
-                    || filename.ends_with(".txt")
+                    || filename.ends_with(".js")
+                    || filename.ends_with(".css")
                     || filename.ends_with(".html")
                     || filename.ends_with(".htm")
                 {
@@ -106,25 +153,30 @@ async fn handler(
 
                 // The f.raw_url is a redirect. So, we need to construct our own here.
                 let contents_url = f.contents_url.as_str();
+                if contents_url.len() < 40 {
+                    continue;
+                }
                 let hash = &contents_url[(contents_url.len() - 40)..];
                 let raw_url = format!(
                     "https://raw.githubusercontent.com/{owner}/{repo}/{}/{}",
-                    hash, &f.filename
+                    hash, filename
                 );
                 let file_uri = Uri::try_from(raw_url.as_str()).unwrap();
                 let mut writer = Vec::new();
-                let _ = Request::new(&file_uri)
+                match Request::new(&file_uri)
                     .method(Method::GET)
                     .header("Accept", "plain/text")
                     .header("User-Agent", "Flows Network Connector")
                     .send(&mut writer)
                     .map_err(|_e| {})
-                    .unwrap();
-                let file_as_text = String::from_utf8_lossy(&writer);
-                if file_as_text.len() < 10 {
-                    // if the file is empty (e.g., a non text file) or very small, we will skip
-                    continue;
+                {
+                    Err(_e) => {
+                        log::error!("Cannot get file");
+                        continue;
+                    }
+                    _ => {}
                 }
+                let file_as_text = String::from_utf8_lossy(&writer);
                 let t_file_as_text = truncate(&file_as_text, CHAR_SOFT_LIMIT);
 
                 resp.push_str("## [");
@@ -133,45 +185,68 @@ async fn handler(
                 resp.push_str(f.blob_url.as_str());
                 resp.push_str(")\n\n");
 
+                log::debug!("Sending file to OpenAI: {}", filename);
                 let co = ChatOptions {
                     model: MODEL,
                     restart: true,
                     system_prompt: Some(system),
-                    retry_times: 3,
                 };
-                let question = "Review the following source code snippet and look for potential problems. Do NOT comment on the completeness of the snippet.\n\n".to_string() + t_file_as_text;
-                // resp.push_str(&question);
-                // resp.push_str("\n\n");
-                if let Some(r) = chat_completion_default_key(&chat_id, &question, &co) {
-                    resp.push_str(&r.choice);
-                    resp.push_str("\n\n");
+                let question = "Review the following source code and look for potential problems. The code might be truncated. So, do NOT comment on the completeness of the source code.\n\n".to_string() + t_file_as_text;
+                match openai.chat_completion(&chat_id, &question, &co).await {
+                    Ok(r) => {
+                        resp.push_str(&r.choice);
+                        resp.push_str("\n\n");
+                        log::debug!("Received OpenAI resp for file: {}", filename);
+                    }
+                    Err(e) => {
+                        log::error!(
+                            "OpenAI returns error for file review for {}: {}",
+                            filename,
+                            e
+                        );
+                    }
                 }
 
+                log::debug!("Sending patch to OpenAI: {}", filename);
                 let co = ChatOptions {
                     model: MODEL,
                     restart: false,
-                    system_prompt: None,
-                    retry_times: 3,
+                    system_prompt: Some(system),
                 };
-                let patch_as_text = f.patch.unwrap();
+                let patch_as_text = f.patch.unwrap_or("".to_string());
                 let t_patch_as_text = truncate(&patch_as_text, CHAR_SOFT_LIMIT);
                 let question = "The following is a patch. Please summarize key changes.\n\n"
                     .to_string()
                     + t_patch_as_text;
-                if let Some(r) = chat_completion_default_key(&chat_id, &question, &co) {
-                    resp.push_str(&r.choice);
-                    resp.push_str("\n\n");
+                match openai.chat_completion(&chat_id, &question, &co).await {
+                    Ok(r) => {
+                        resp.push_str(&r.choice);
+                        resp.push_str("\n\n");
+                        log::debug!("Received OpenAI resp for patch: {}", filename);
+                    }
+                    Err(e) => {
+                        log::error!(
+                            "OpenAI returns error for patch review for {}: {}",
+                            filename,
+                            e
+                        );
+                    }
                 }
             }
         }
         Err(_error) => {
-            write_error_log!("Cannot get file list");
+            log::error!("Cannot get file list");
         }
     }
 
     // Send the entire response to GitHub PR
-    let issues = octo.issues(owner, repo);
-    issues.create_comment(pull_number, resp).await.unwrap();
+    // issues.create_comment(pull_number, resp).await.unwrap();
+    match issues.update_comment(comment_id, resp).await {
+        Err(error) => {
+            log::error!("Error posting resp: {}", error);
+        }
+        _ => {}
+    }
 }
 
 fn truncate(s: &str, max_chars: usize) -> &str {
