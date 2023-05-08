1d80e90e66091a60e6b0abd7a860342259a0513a
DarumaDocker <DarumaDocker@gmail.com>
2023-05-08 18:38:38 +0800

    test for new github sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use github_flows::{
     get_octo, listen_to_event,
     octocrab::models::{events::payload::EventPayload, reactions::ReactionContent},
+    GithubLogin,
 };
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     listen_to_event(
+        &GithubLogin::Default,
         "DarumaDockerDev",
         "github-func-test",
         vec!["issue_comment"],
@@ -20,7 +22,7 @@ async fn handler(payload: EventPayload) {
         let issue_number = e.comment.id.0;
 
         // installed app login
-        let octo = get_octo(Some(String::from("DarumaDockerDev")));
+        let octo = get_octo(&GithubLogin::Default);
 
         let _reaction = octo
             .issues("DarumaDockerDev", "github-func-test")
