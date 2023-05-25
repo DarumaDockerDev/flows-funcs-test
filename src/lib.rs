7cb677984adac70396788514dfd634d086b15426
DarumaDocker <DarumaDocker@gmail.com>
2023-05-25 18:30:35 +0800

    test for discord 0.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-use discord_flows::{get_client, listen_to_event, model::Message};
+use discord_flows::{get_client, listen_to_event, model::Message, Bot};
 use flowsnet_platform_sdk::logger;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     logger::init();
-    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    // let token = std::env::var("DISCORD_TOKEN").unwrap();
 
-    listen_to_event(token.clone(), move |msg| handle(msg, token)).await;
+    listen_to_event(Bot::Default, move |msg| handle(msg)).await;
 }
 
-async fn handle(msg: Message, token: String) {
-    let client = get_client(token);
+async fn handle(msg: Message) {
+    let client = get_client(Bot::Default);
     let channel_id = msg.channel_id;
     let content = msg.content;
 
