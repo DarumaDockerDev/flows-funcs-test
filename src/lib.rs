f40db249acc0b123239c89382f31ffb0e1eabee2
DarumaDocker <DarumaDocker@gmail.com>
2023-06-06 13:45:40 +0800

    test for new discord

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use discord_flows::{get_client, listen_to_event, model::Message, Bot};
+use discord_flows::{get_client, listen_to_event, model::Message, Bot, ChannelId};
 use flowsnet_platform_sdk::logger;
+
+const CHANNEL_ID: u64 = 995948764542013493;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -7,11 +9,14 @@ pub async fn run() {
     logger::init();
     // let token = std::env::var("DISCORD_TOKEN").unwrap();
 
-    listen_to_event(Bot::Default, move |msg| handle(msg)).await;
+    listen_to_event(Bot::default_bot(ChannelId(CHANNEL_ID)), move |msg| {
+        handle(msg)
+    })
+    .await;
 }
 
 async fn handle(msg: Message) {
-    let client = get_client(Bot::Default);
+    let client = get_client(Bot::default_bot(ChannelId(CHANNEL_ID)));
     let channel_id = msg.channel_id;
     let content = msg.content;
 
