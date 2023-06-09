51c7cd2887a9591c09766626e8df66f8590c145b
DarumaDocker <DarumaDocker@gmail.com>
2023-06-09 15:15:31 +0800

    test for web-scraper

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use std::{collections::HashMap, thread, time::Duration};
+use std::collections::HashMap;
 
-use http_req::request;
 use lambda_flows::{request_received, send_response};
-use serde::Deserialize;
 use serde_json::Value;
+use web_scraper_flows::get_page_text;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -12,101 +11,24 @@ pub async fn run() {
 }
 
 async fn handler(qry: HashMap<String, Value>, _body: Vec<u8>) {
-    println!(
-        "in wasm. before request {}",
-        qry.get("x").unwrap_or(&Value::String(String::from("-")))
-    );
-    /*
-    let mut writer = Vec::new();
-    request::get(
-        "http://127.0.0.1:8094/lambda/aPz1iwP6r4?city=beijing",
-        &mut writer,
-    );
-    */
-    thread::sleep(Duration::from_secs(10));
+    let url = qry.get("url").expect("No url provided").as_str().unwrap();
 
-    println!(
-        "in wasm. passed request {}",
-        qry.get("x").unwrap_or(&Value::String(String::from("-")))
-    );
-
-    let city = qry.get("city").unwrap_or(&Value::Null).as_str();
-    let resp = match city {
-        Some(c) => get_weather(c).map(|w| {
-            format!(
-                "Today: {},
-Low temperature: {} °C,
-High temperature: {} °C,
-Wind Speed: {} km/h",
-                w.weather
-                    .first()
-                    .unwrap_or(&Weather {
-                        main: "Unknown".to_string()
-                    })
-                    .main,
-                w.main.temp_min as i32,
-                w.main.temp_max as i32,
-                w.wind.speed as i32
-            )
-        }),
-        None => Err(String::from("No city in query")),
-    };
-
-    match resp {
-        Ok(r) => send_response(
+    match get_page_text(url).await {
+        Ok(text) => send_response(
             200,
             vec![(
                 String::from("content-type"),
-                String::from("text/html; charset=UTF-8"),
+                String::from("text/plain; charset=UTF-8"),
             )],
-            r.as_bytes().to_vec(),
+            text.as_bytes().to_vec(),
         ),
         Err(e) => send_response(
             400,
             vec![(
                 String::from("content-type"),
-                String::from("text/html; charset=UTF-8"),
+                String::from("text/plain; charset=UTF-8"),
             )],
             e.as_bytes().to_vec(),
         ),
     }
-}
-
-#[derive(Deserialize)]
-struct ApiResult {
-    weather: Vec<Weather>,
-    main: Main,
-    wind: Wind,
-}
-
-#[derive(Deserialize)]
-struct Weather {
-    main: String,
-}
-
-#[derive(Deserialize)]
-struct Main {
-    temp_max: f64,
-    temp_min: f64,
-}
-
-#[derive(Deserialize)]
-struct Wind {
-    speed: f64,
-}
-
-fn get_weather(city: &str) -> Result<ApiResult, String> {
-    let mut writer = Vec::new();
-    let api_key = std::env::var("API_KEY").unwrap();
-    let query_str = format!(
-        "https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
-    );
-
-    request::get(query_str, &mut writer)
-        .map_err(|e| e.to_string())
-        .and_then(|_| {
-            serde_json::from_slice::<ApiResult>(&writer).map_err(|_| {
-                "Please check if you've typed the name of your city correctly".to_string()
-            })
-        })
 }
