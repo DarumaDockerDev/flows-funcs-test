c96df9e2490990f9199034c764e09842410c51a5
DarumaDocker <DarumaDocker@gmail.com>
2023-06-14 07:55:50 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use openai_flows::{chat_completion, ChatOptions};
 use slack_flows::{listen_to_channel, send_message_to_channel};
 
 #[no_mangle]
 pub fn run() {
-    listen_to_channel("family-wangshi", "chat", |sm| {
-        send_message_to_channel("family-wangshi", "chat", "---".to_string());
-    });
+    listen_to_channel("family-wangshi", "chat", |sm| handler());
+}
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+async fn handler() {
+    send_message_to_channel("family-wangshi", "chat", "---".to_string());
 }
