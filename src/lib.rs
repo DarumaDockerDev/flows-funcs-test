75d99a655ece4d4a5bfd9a214fb50e44e2342185
DarumaDocker <DarumaDocker@gmail.com>
2023-06-14 07:46:23 +0800

    test for slack leading segfault

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,6 @@ use slack_flows::{listen_to_channel, send_message_to_channel};
 #[no_mangle]
 pub fn run() {
     listen_to_channel("family-wangshi", "chat", |sm| {
-        let co = ChatOptions {
-            restart: sm.text.eq_ignore_ascii_case("/restart"),
-            restarted_sentence: Some("let's change the subject"),
-        };
-        if let Some(r) = chat_completion("Michael", "any_conversation", &sm.text, &co) {
-            send_message_to_channel("family-wangshi", "chat", r.choice);
-        }
+        send_message_to_channel("family-wangshi", "chat", "---".to_string());
     });
 }
