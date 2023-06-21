9ba425be7e392c0a3bb5fd5c6e21ddf734128ff2
DarumaDocker <DarumaDocker@gmail.com>
2023-06-21 11:19:08 +0800

    update tg

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,36 @@
-use openai_flows::{chat_completion, ChatModel, ChatOptions};
+use openai_flows::{chat, OpenAIFlows};
 use tg_flows::{listen_to_update, Telegram, UpdateKind};
 
 #[no_mangle]
-pub fn run() {
+#[tokio::main(flavor = "current_thread")]
+pub async fn run() {
     let openai_key_name = "Michael";
 
     let telegram_token = std::env::var("telegram_token").unwrap();
     let tele = Telegram::new(telegram_token.clone());
 
-    listen_to_update(telegram_token, |update| {
-        if let UpdateKind::Message(msg) = update.kind {
-            let text = msg.text().unwrap_or("");
-            let chat_id = msg.chat.id;
+    listen_to_update(telegram_token, |update| handler(update, &tele));
+}
 
-            let c = chat_completion(
-                &openai_key_name,
-                &chat_id.to_string(),
-                &text,
-                &ChatOptions {
-                    model: ChatModel::GPT35Turbo,
-                    restart: false,
-                    system_prompt: Some("You are awesome!"),
-                    retry_times: 2,
-                },
-            );
+async fn handler(update: tg_flows::Update, tele: &Telegram) {
+    if let UpdateKind::Message(msg) = update.kind {
+        let text = msg.text().unwrap_or("");
+        let chat_id = msg.chat.id;
 
-            if let Some(c) = c {
-                if c.restarted {
-                    _ = tele.send_message(chat_id, "Let's start a new conversation!");
-                }
+        let of = OpenAIFlows::new();
+        let co = chat::ChatOptions::default();
 
-                // _ = tele.edit_message_text(chat_id, m.id, c.choice);
-                _ = tele.send_message(chat_id, c.choice);
-            } else {
-                _ = tele.send_message(chat_id, "I have no choice");
+        let c = of.chat_completion(&chat_id.to_string(), &text, &co).await;
+
+        if let Ok(c) = c {
+            if c.restarted {
+                _ = tele.send_message(chat_id, "Let's start a new conversation!");
             }
+
+            // _ = tele.edit_message_text(chat_id, m.id, c.choice);
+            _ = tele.send_message(chat_id, c.choice);
+        } else {
+            _ = tele.send_message(chat_id, "I have no choice");
         }
-    });
+    }
 }
