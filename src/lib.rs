33d4d298d15070833d12d2952afd80bef518fae2
DarumaDocker <DarumaDocker@gmail.com>
2023-06-30 11:44:33 +0800

    async schedule

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,11 @@ use flowsnet_platform_sdk::write_error_log;
 use schedule_flows::schedule_cron_job;
 
 #[no_mangle]
-pub fn run() {
-    schedule_cron_job(String::from("7 * * * *"), String::from("no"), handler);
+#[tokio::main(flavor = "current_thread")]
+pub async fn run() {
+    schedule_cron_job(String::from("7 * * * *"), String::from("no"), handler).await;
 }
 
-fn handler(payload: Vec<u8>) {
+async fn handler(payload: Vec<u8>) {
     write_error_log!(payload);
 }
