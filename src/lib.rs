8db839257d464dbfe40ee67a0262af7fdbf7bacf
DarumaDocker <DarumaDocker@gmail.com>
2023-07-24 16:35:13 +0800

    simple slack

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,16 @@
-use dotenv::dotenv;
-use openai_flows::{
-    chat::{ChatModel, ChatOptions},
-    OpenAIFlows,
-};
 use slack_flows::{listen_to_channel, send_message_to_channel, SlackMessage};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    dotenv().ok();
-    listen_to_channel("family-wangshi", "chat", |sm| handler(sm)).await;
+    let workspace = std::env::var("SLACK_WORKSPACE").unwrap();
+    let channel = std::env::var("SLACK_CHANNEL").unwrap();
+    listen_to_channel(workspace.as_str(), channel.as_str(), |sm| {
+        handler(workspace.as_str(), channel.as_str(), sm)
+    })
+    .await;
 }
 
-async fn handler(sm: SlackMessage) {
-    let mut openai = OpenAIFlows::new();
-    openai.set_retry_times(3);
-
-    let co = ChatOptions {
-        model: ChatModel::GPT35Turbo,
-        restart: false,
-        system_prompt: None,
-    };
-
-    match openai
-        .chat_completion("any_conversation", &sm.text, &co)
-        .await
-    {
-        Ok(r) => {
-            send_message_to_channel("family-wangshi", "chat", r.choice).await;
-        }
-        Err(e) => {
-            send_message_to_channel("family-wangshi", "chat", e.to_string()).await;
-        }
-    }
+async fn handler(workspace: &str, channel: &str, sm: SlackMessage) {
+    send_message_to_channel(workspace, channel, sm.text).await;
 }
