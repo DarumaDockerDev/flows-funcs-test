4c856779daa8f97b662cdba48d7aa92f9534e9c2
DarumaDocker <DarumaDocker@gmail.com>
2023-08-06 16:43:04 +0800

    test for llm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, thread, time::Duration};
+use std::collections::HashMap;
 
-use http_req::request;
+use llmservice_flows::{chat::ChatOptions, LLMServiceFlows};
+
 use lambda_flows::{request_received, send_response};
 use serde_json::Value;
 
@@ -10,32 +11,36 @@ pub async fn run() {
     request_received(handler).await;
 }
 
-async fn handler(_qry: HashMap<String, Value>, _body: Vec<u8>) {
-    // thread::sleep(Duration::from_secs(10));
+async fn handler(_qry: HashMap<String, Value>, body: Vec<u8>) {
+    let co = ChatOptions {
+        model: Some("gpt-3.5-turbo"),
+        token_limit: 4096,
+        ..Default::default()
+    };
+    let api_key = std::env::var("OPENAI_API_KEY").unwrap();
+    let lf = LLMServiceFlows::new(
+        "https://api.openai.com/v1/chat/completions",
+        api_key.as_str(),
+    );
 
-    let mut writer = Vec::new();
-    let query_str = format!("https://hub.dummyapis.com/delay?seconds=3");
+    let r = match lf
+        .chat_completion(
+            "example_conversion_1",
+            String::from_utf8_lossy(&body).into_owned().as_str(),
+            &co,
+        )
+        .await
+    {
+        Ok(c) => c.choice,
+        Err(e) => e,
+    };
 
-    let resp = request::get(query_str, &mut writer)
-        .map_err(|e| e.to_string())
-        .and_then(|_| String::from_utf8(writer).map_err(|_| "Unexpected error".to_string()));
-
-    match resp {
-        Ok(r) => send_response(
-            200,
-            vec![(
-                String::from("content-type"),
-                String::from("text/html; charset=UTF-8"),
-            )],
-            r.as_bytes().to_vec(),
-        ),
-        Err(e) => send_response(
-            400,
-            vec![(
-                String::from("content-type"),
-                String::from("text/html; charset=UTF-8"),
-            )],
-            e.as_bytes().to_vec(),
-        ),
-    }
+    send_response(
+        200,
+        vec![(
+            String::from("content-type"),
+            String::from("text/plain; charset=UTF-8"),
+        )],
+        r.as_bytes().to_vec(),
+    );
 }
