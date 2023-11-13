afea9673f5d297055f4c930941f2af192abbd1ef
DarumaDocker <DarumaDocker@gmail.com>
2023-11-13 12:46:07 +0800

    Test for OpenAI assistant

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
+use async_openai_wasi::{
+    types::{
+        CreateMessageRequestArgs, CreateRunRequestArgs, CreateThreadRequestArgs, MessageContent,
+    },
+    Client,
+};
 use flowsnet_platform_sdk::logger;
-use openai_flows::{chat, OpenAIFlows};
 use tg_flows::{listen_to_update, update_handler, Telegram, UpdateKind};
-use webhook_flows::create_endpoint;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn on_deploy() {
+    logger::init();
+
+    create_thread().await;
+
     let telegram_token = std::env::var("telegram_token").unwrap();
     listen_to_update(telegram_token).await;
-    create_endpoint().await;
 }
 
 #[update_handler]
@@ -21,35 +28,71 @@ async fn handler(update: tg_flows::Update) {
         let text = msg.text().unwrap_or("");
         let chat_id = msg.chat.id;
 
-        let of = OpenAIFlows::new();
-        let mut co = chat::ChatOptions {
-            max_tokens: Some(50),
-            post_prompt: Some("Do not give me any information about procedures and service features that are not mentioned in the PROVIDED CONTEXT."),
-            ..chat::ChatOptions::default()
-        };
-        log::debug!("chat id: {}", chat_id);
+        let response = run_message(String::from(text)).await;
 
-        if text == "/restart" {
-            co.restart = true;
-        }
+        _ = tele.send_message(chat_id, response);
+    }
+}
 
-        if let Some(history) = chat::chat_history(&chat_id.to_string(), 10) {
-            log::debug!("chat history: {:?}", history);
-        }
+async fn create_thread() {
+    let client = Client::new();
+    let thread_id = std::env::var("WASMEDGE_THREAD_ID").unwrap();
 
-        log::debug!("Received msg {} @{}", text, chat_id);
+    if let Err(_) = client.threads().retrieve(thread_id.as_str()).await {
+        let create_thread_request = CreateThreadRequestArgs::default().build().unwrap();
 
-        let c = of.chat_completion(&chat_id.to_string(), &text, &co).await;
-
-        if let Ok(c) = c {
-            if c.restarted {
-                _ = tele.send_message(chat_id, "Let's start a new conversation!");
+        match client.threads().create(create_thread_request).await {
+            Ok(to) => {
+                log::info!("New thread (ID: {}) created.", to.id);
             }
-
-            // _ = tele.edit_message_text(chat_id, m.id, c.choice);
-            _ = tele.send_message(chat_id, c.choice);
-        } else {
-            _ = tele.send_message(chat_id, "I have no choice");
+            Err(e) => {
+                log::error!("Failed to create thread: {:?}", e);
+            }
         }
     }
+}
+
+async fn run_message(text: String) -> String {
+    let client = Client::new();
+    let assistant_id = std::env::var("WASMEDGE_ASSISTANT_ID").unwrap();
+    let thread_id = std::env::var("WASMEDGE_THREAD_ID").unwrap();
+
+    let mut create_message_request = CreateMessageRequestArgs::default().build().unwrap();
+    create_message_request.content = text;
+    let message_object = client
+        .threads()
+        .messages(&thread_id)
+        .create(create_message_request)
+        .await
+        .unwrap();
+
+    log::debug!("message object: {message_object:#?}");
+
+    let mut create_run_request = CreateRunRequestArgs::default().build().unwrap();
+    create_run_request.assistant_id = assistant_id;
+    let run_object = client
+        .threads()
+        .runs(&thread_id)
+        .create(create_run_request)
+        .await
+        .unwrap();
+
+    println!("run object: {run_object:#?}");
+
+    let mut thread_messages = client
+        .threads()
+        .messages(&thread_id)
+        .list(&[("limit", "1")])
+        .await
+        .unwrap();
+
+    println!("thread messages: {thread_messages:#?}");
+
+    let c = thread_messages.data.pop().unwrap();
+    let c = c.content.into_iter().filter_map(|x| match x {
+        MessageContent::Text(t) => Some(t.text.value),
+        _ => None,
+    });
+
+    c.collect()
 }
