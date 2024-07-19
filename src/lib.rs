8850e960ac759260b27245424377fbbbc82aa165
DarumaDocker <DarumaDocker@gmail.com>
2024-07-19 17:34:23 +0800

    Use patch for tokio and other crates

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,6 @@ use airtable_api::{api_key_from_env, Airtable, Record};
 use flowsnet_platform_sdk::logger;
 use std::collections::HashMap;
 
-// use http_req::request;
-use http_req::{
-    request::{self, Method, Request},
-    uri::Uri,
-};
 use openai_flows::{
     chat::{self, ChatModel, ChatOptions, ResponseFormat, ResponseFormatType},
     OpenAIFlows,
@@ -15,6 +10,7 @@ use sendgrid::v3::*;
 
 use serde::Deserialize;
 use serde_json::Value;
+use tokio_postgres::NoTls;
 use webhook_flows::{
     create_endpoint, request_handler,
     route::{get, options, post, route, RouteError, Router},
@@ -38,6 +34,7 @@ async fn handler() {
     router.insert("/email", vec![post(email)]).unwrap();
     router.insert("/airtable", vec![post(airtable)]).unwrap();
     router.insert("/test", vec![get(test)]).unwrap();
+    router.insert("/postgres", vec![get(postgres)]).unwrap();
 
     if let Err(e) = route(router).await {
         match e {
@@ -51,36 +48,38 @@ async fn handler() {
     }
 }
 
+async fn postgres(_headers: Vec<(String, String)>, _qry: HashMap<String, Value>, _body: Vec<u8>) {
+    let (client, connection) = tokio_postgres::connect("postgres://postgres.cvlrxycwyxkjtshjcuyq:Za0BHyJ595UPab94@aws-0-us-west-1.pooler.supabase.com:5432/postgres", NoTls).await.unwrap();
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let row: String = client
+        .query_one("SELECT $1::TEXT", &[&"hello world"])
+        .await
+        .unwrap()
+        .get(0);
+
+    send_response(200, vec![], row.into_bytes());
+}
+
 async fn test(_headers: Vec<(String, String)>, _qry: HashMap<String, Value>, _body: Vec<u8>) {
     logger::init();
 
-    let query = format!(
-        "{}?chain={:#x}",
-        "0x70655c1c0D7Ef1273D17E5610a2079E7465F0FF6", 80001u64
-    )
-    .to_string();
-    let api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImEyZjZkMDAxLTQzODktNGZhMS04MzQxLTIyZjNlYTBkMGE0NiIsIm9yZ0lkIjoiMzY3OTk5IiwidXNlcklkIjoiMzc4MjEyIiwidHlwZUlkIjoiYWE5MzRjMWItODI0Zi00NGQ5LWI2OTYtNjMwOWE3YjJkYzQ5IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDI0NDc3MTQsImV4cCI6NDg1ODIwNzcxNH0.r5211T8RJZI9j0gQDWNR0oflRSHMGz1AOGwl1RRGzlc";
-    log::info!("get_request: {} {}", query, api_key);
-    let url = format!("https://deep-index.moralis.io/api/v2.2/{}", query);
-
-    let addr = Uri::try_from(url.as_str()).unwrap();
-    let mut writer = Vec::new();
-
-    let response = Request::new(&addr)
-        .method(Method::GET)
-        .header("Connection", "Close")
-        .header("accept", "application/json")
-        .header("X-API-Key", api_key)
-        .send(&mut writer)
-        .unwrap();
+    let response =
+        reqwest::get("https://github.com/juntao/test/blob/test1/src/github-pr-summary.rs")
+            .await
+            .unwrap();
 
     send_response(
-        response.status_code().into(),
-        vec![(
-            String::from("content-type"),
-            String::from("application/json"),
-        )],
-        writer.to_vec(),
+        response.status().into(),
+        vec![(String::from("content-type"), String::from("plain/text"))],
+        response.bytes().await.unwrap().to_vec(),
     );
 }
 
@@ -202,7 +201,7 @@ async fn query(
 
     let city = qry.get("city").unwrap_or(&Value::Null).as_str();
     let resp = match city {
-        Some(c) => get_weather(c).map(|w| {
+        Some(c) => get_weather(c).await.map(|w| {
             format!(
                 "Today: {},
 Low temperature: {} °C,
@@ -267,18 +266,17 @@ struct Wind {
     speed: f64,
 }
 
-fn get_weather(city: &str) -> Result<ApiResult, String> {
-    let mut writer = Vec::new();
+async fn get_weather(city: &str) -> Result<ApiResult, String> {
     let api_key = "09a55b004ce2f065b903015e3284de35";
     let query_str = format!(
         "https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
     );
 
-    request::get(query_str, &mut writer)
-        .map_err(|e| e.to_string())
-        .and_then(|_| {
-            serde_json::from_slice::<ApiResult>(&writer).map_err(|_| {
+    match reqwest::get(query_str).await {
+        Ok(res) => serde_json::from_slice::<ApiResult>(res.bytes().await.unwrap().as_ref())
+            .map_err(|_| {
                 "Please check if you've typed the name of your city correctly".to_string()
-            })
-        })
+            }),
+        Err(e) => return Err(e.to_string()),
+    }
 }
